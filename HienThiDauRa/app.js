ee3594fb98ff36ee44e5d74ec4164f96395029eb
pikachucodegym <hoanglien89nd@gmail.com>
2020-02-23 11:21:06 +0700

    HienThiDauRa

---
+++ HienThiDauRa/app.js
@@ -0,0 +1,9 @@
+ function ShowMessage(){
+         alert('Hiển thị kết quả sử dụng hàm alert():...!');
+}
+    function ShowMessage1() {
+          document.getElementById('one').innerHTML=' Hiển thị kết quả sử dụng thẻ HTML:';
+               }
+
+
+
